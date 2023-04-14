86f370591f87e4fe9ee7b1ef962b5a0ce0b3b2d3
Synnly <emanuelfernandes54@gmail.com>
2023-04-14 22:45:08 +0200

    Ajout de la verification de la presence du mot "TEST" dans le message

--- dechiffreMessage.c
+++ dechiffreMessage.c
@@ -5,6 +5,7 @@
 #include <sys/fcntl.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include <string.h>
 
 /**
  * @brief Dechiffre un caractère selon le décalage
@@ -35,16 +36,35 @@ int main (int argc, char** argv){
 
     int tube_l = atoi(argv[1]); // descripteur du tube de lecture
     int tube_e = atoi(argv[2]); // descripteur du tube d'écriture
-    char lettre;
+    char lettre, buffer;
+    char mot[128] = "\0";       // Table qui stocke le mot qu'on lit
+    char* motAChercher = "TEST"; // Mot à chercher
+    bool aTrouveMot = false;    // Booléen qui indique si on a trouvé le mot à chercher
+
 
     close(tube_e);  // fermeture du tube d'écriture
 
     // lecture des caracteres
     while(read(tube_l, &lettre, sizeof(char)) > 0){
-        printf("%c", dechiffrerChar(lettre, 2));
-    }
-    printf("\n");
 
-    close(tube_l);  // fermeture du tube de lecture
+        lettre = dechiffrerChar(lettre, 2);     // On déchiffre le caractère
+        printf("%c", lettre);                   // On affiche le caractère déchiffré
+        buffer = (lettre == ' ' || lettre == '.' || lettre == ',' ? '\0' : lettre);  // On met le caractère \0 dans le buffer si ce n'est pas une lettre
+        strcat(mot, &buffer);   // On ajoute le caractère au mot
+
+        // On verifie si on a touvé le mot à chercher et on vide le mot
+        if(buffer == '\0'){
+
+            if(strcmp(mot, motAChercher) == 0){
+                aTrouveMot = true;
+            }
+
+            mot[0] = '\0';
+        }
+    }
+    // On affiche si on a trouvé le mot à chercher
+    printf("\nLe mot %s a été trouvé : %s\n", motAChercher, (aTrouveMot ? "OUI" : "NON"));
+
+    close(tube_l);  // Fermeture du tube de lecture
     return 0;
 }
