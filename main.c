93bcfce8b39011a92133193530bbc8a0032f58e9
Synnly <emanuelfernandes54@gmail.com>
2023-04-16 22:26:16 +0200

    DE GRACE CA MARCHE

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <sys/fcntl.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#define NB_FILS 27
 
 /**
  * @brief Affiche le nombre de caractères du message et le retourne
@@ -67,10 +68,14 @@ bool fichierEstChiffre(int descr){
     return (lettreC == 'C' && lettreR == 'R');
 }
 
-int main (int argc, char** argv){
-
-    int descr = open(argv[1], O_RDONLY);  // Ouverture du fichier en lecture seule
-
+/**
+ * @brief Verifie que le fichier a une entete valide
+ * @param descr Le descripteur du fichier
+ * @param nomFichier Le nom du fichier
+ * @param tailleMessage La taille du message
+ * @param nbOctetsAvantMsg Le nombre d'octets avant le message
+ */
+void verificationFichier(int descr, char* nomFichier, int* tailleMessage, int* nbOctetsAvantMsg){
     // Vérification de l'ouverture du fichier
     if (descr == -1){
         perror("Erreur lors de l'ouverture du fichier");
@@ -79,52 +84,64 @@ int main (int argc, char** argv){
 
     // Verification du chiffrage du fichier
     if(fichierEstChiffre(descr)){
-        printf("Le fichier %s est bien un fichier chiffré\n", argv[1]);
+        printf("Le fichier %s est bien un fichier chiffré\n", nomFichier);
     }
     else{
-        printf("ERREUR : Le fichier %s n'est pas chiffré\n", argv[1]);
+        printf("ERREUR : Le fichier %s n'est pas chiffré\n", nomFichier);
         close(descr);
         exit(EXIT_FAILURE);
     }
-    
-    int tailleMessage = nbCharMessage(descr, argv[1]);  // Lecture de la taille du message
-    int nbOctetsAvantMsg = nbOctets(descr, argv[1]);    // Lecture du nombre d'octets avant le message
+
+    *tailleMessage = nbCharMessage(descr, nomFichier);  // Lecture de la taille du message
+    *nbOctetsAvantMsg = nbOctets(descr, nomFichier);    // Lecture du nombre d'octets avant le message
 
     // Verification de la taille du message et du nombre d'octets avant le message
-    if (nbOctetsAvantMsg < 0 || tailleMessage < 0 || nbOctetsAvantMsg + tailleMessage > getTailleFichier(descr)-10){
+    if (*nbOctetsAvantMsg < 0 || *tailleMessage < 0 || *nbOctetsAvantMsg + *tailleMessage > getTailleFichier(descr)-10){
         printf("ERREUR : Le fichier a mal été chiffré\n");
         close(descr);
         exit(EXIT_FAILURE);
     }
+}
+
+int main (int argc, char** argv){
+
+    int descr = open(argv[1], O_RDONLY);  // Ouverture du fichier en lecture seule
+
+    int tailleMessage, nbOctetsAvantMsg;
+    verificationFichier(descr, argv[1], &tailleMessage, &nbOctetsAvantMsg);
     
     lseek(descr, nbOctetsAvantMsg, SEEK_CUR);   // Deplacement du curseur au debut du message
+    
+    int tubes[NB_FILS][2];  // Tableau de tubes
+    char tube_l[10];        // Descripteur de lecture du tube
+    char decalage[3];       // Decalage du message
+    pid_t tabFils[NB_FILS]; // Tableau des PID des fils
 
-    // Creation des tubes
-    int tubes[2];
-    pipe(tubes);  
+    for(int i = 0; i < NB_FILS; i++){
+        pipe(tubes[i]);     // Creation du tube
 
-    // Conversion des descripteurs du tube en chaine de caractères
-    char tube_l[10];
-    sprintf(tube_l, "%d", tubes[0]);
+        // Conversions en chaine de caractères
+        sprintf(tube_l, "%d", tubes[i][0]);
+        sprintf(decalage, "%d", i+1);
 
-    // Creation du processus fils
-    pid_t pid;
-    switch (pid = fork()){
-        case (pid_t) -1:
-            perror("ERREUR: fork invalide");
-            exit(EXIT_FAILURE);
-            break;
+        // Creation du processus fils
+        switch (tabFils[i] = fork()){
+            case (pid_t) -1:
+                perror("ERREUR: fork invalide");
+                exit(EXIT_FAILURE);
+                break;
 
-        case (pid_t) 0:         // Processus fils
-            close(tubes[1]);    // Fermeture du descripteur d'ecriture du tube
-            execl("./dechiffreMessage", "dechiffreMessage", tube_l, argv[2], argv[3], NULL);
-            perror("ERREUR: recouvrement impossible");
-            exit(EXIT_FAILURE);
-            break;
+            case (pid_t) 0:             // Processus fils
+                close(tubes[i][1]);     // Fermeture du descripteur d'ecriture du tube
+                execl("./dechiffreMessage", "dechiffreMessage", tube_l, argv[2], decalage, NULL);
+                perror("ERREUR: recouvrement impossible");
+                exit(EXIT_FAILURE);
+                break;
 
-        default:                // Processus père
-            close(tubes[0]);    // Fermeture du descripteur de lecture du tube
-            break;
+            default:                    // Processus père
+                close(tubes[i][0]);     // Fermeture du descripteur de lecture du tube
+                break;
+        }
     }
 
 
@@ -132,16 +149,26 @@ int main (int argc, char** argv){
     char lettre;
     for(int i = 0; i < tailleMessage; i++){
         read(descr, &lettre, sizeof(char));
-        write(tubes[1], &lettre, sizeof(char));
+
+        // Ecriture dans les tubes
+        for(int j = 0; j < NB_FILS; j++){
+            write(tubes[j][1], &lettre, sizeof(char));
+        }
     }
 
-    close(tubes[1]);    // Fermeture du descripteur d'ecriture du tube
+    // Fermeture du descripteur d'ecriture des tubes
+    for(int i = NB_FILS; i >= 0 ; i--){
+        close(tubes[i][1]); 
+    }
 
     // Attente de la fin du processus fils
     int pStatus;
-    wait(&pStatus);
-    if(WEXITSTATUS(pStatus)){
-        printf("Fichier %s déchiffré avec pour décalage %s\n", argv[1], argv[3]);
+
+    for(int i = 0; i < NB_FILS; i++){
+        waitpid(tabFils[i], &pStatus, 0);
+        if(WEXITSTATUS(pStatus)){
+            printf("Fichier %s déchiffré avec pour décalage %d\n", argv[1], i+1);
+        }
     }
 
     close(descr); // Fermeture du fichier
